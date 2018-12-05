b1b9ad64d99562ef815fc19a39527128ffa6df3d
Ethan Plant <plant.ethan@gmail.com>
2018-12-05 12:56:01 -0500

    Fix server port

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 let players = [];
 
-server.listen(8080, () => {
-    console.log('Server is now running');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log('Server is now running on ' + PORT);
 });
 
 io.on('connection', (socket) => {
